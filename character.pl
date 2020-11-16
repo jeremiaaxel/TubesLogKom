10571efa78087497559941e38b752f1dd22f6a88
fluffyotter <blcktmyie123@gmail.com>
2020-11-16 15:36:11 +0700

    Add zip file for Asistensi 1

---
+++ character.pl
@@ -0,0 +1,30 @@
+dynamic(inventory/1).
+dynamic(currHP/1)
+
+/* Job: swordsman, archer, sorcerer */
+job(characterX,swordsman).
+job(characterX,archer).
+job(characterX,sorcerer).
+
+/* HP */
+hp(characterX,hpX).
+
+/* Attack */
+
+
+/* Defense */
+
+
+/* Equipment */
+
+
+/* Starter Pack - dapet 5 heatlh potion */
+/* Bisa naik level: status attack, defense, max hp naik */
+/* level up: X xp, X growth rate */
+/* Hanya dapat menggunakan equipment sesuai dengan job */
+
+
+/*  */
+
+/* Facts */
+/* Rules */
