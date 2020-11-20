8aef4eeee162edab38f5097bf2e064598562854e
jonathan-cj <13519144@std.stei.itb.ac.id>
2020-11-20 15:53:21 +0700

    Item sama store

---
+++ item.pl
@@ -0,0 +1,100 @@
+/* Items */
+/* Facts */
+:- dynamic(weapon/1).
+:- dynamic(armor/1).
+:- dynamic(accessory/1).
+:- dynamic(potion/2).
+:- dynamic(gold/1).
+
+/*itemList([woodSword,stoneSword,ironSword,shortBow,longBow,
+    crossBow,fireBall,iceRay,thunderStrike,leatherArmor,ironArmor]).*/
+
+itemList([['Wood','Sword'],['Stone','Sword'],['Iron','Sword'],['Short','Bow'],
+    ['Long','Bow'],['Cross','Bow'],['Fireball','Spellscroll'],['Iceray','Spellscroll'],
+    ['Thunderstrike','Spellscroll'],['Leather','Armor'],['Iron','Armor']]).
+
+/* Rules */
+/*Basis*/
+back([],_) :- !.
+back([Head|[]],R) :- !,R=Head.
+/*Rekurens*/
+back([_|Tail],R) :- back(Tail,R1),R=R1.
+
+/* Insert Item ke inventory*/
+/* armor([1,'Leather','Armor']). */
+
+checkItem([N,Type,Z],Y) :- 
+    (weapon([X,Type,Z]);armor([X,Type,Z]);accessory([X,Type,Z])),!,
+    retract(weapon([X,Type,Z]));retract(armor([X,Type,Z]));retract(accessory([X,Type,Z])),
+    Y is X+N.
+checkItem([N,_,_],Y) :- 
+    Y is N.
+
+insert(Item,Equip) :- 
+    back(Item,Type),
+    (Type='Sword'; Type='Bow'; Type='Spellscroll'),!,
+    assertz(weapon(Item)),
+    Equip=yes,
+    equipItem(weapon(Item)).
+insert(Item,Equip) :- 
+    back(Item,Type),
+    Type='Armor',!,
+    assertz(armor(Item)),
+    Equip=yes,
+    equipItem(armor(Item)).
+insert(Item,Equip) :- 
+    back(Item,Type),
+    (Type='Ring'; Type='Amulet'),!,
+    assertz(accessory(Item)),
+    Equip=yes,
+    equipItem(amulet(Item)).
+
+/* Equip item */
+/* Untuk periksa item yang di-equip player */
+:- dynamic(equip/1).
+
+/* equip(armor([1,'Leather','Armor'])). */
+
+equipItem(weapon([N,Type,X])) :- 
+    retract(equip(weapon([_,Type1,X]))),
+    assertz(equip(weapon([N,Type,X]))),
+    write('Removing '),printlist([Type1,X]),write(' to equip '),printlist([Type,X]).
+equipItem(armor([N,Type,X])) :- 
+    retract(equip(armor([_,Type1,X]))),
+    assertz(equip(armor([N,Type,X]))),
+    write('Removing '),printlist([Type1,X]),write(' to equip '),printlist([Type,X]).
+equipItem(accessory([N,Type,X])) :- 
+    retract(equip(accessory([_,Type1,X]))),
+    assertz(equip(accessory([N,Type,X]))),
+    write('Removing '),printlist([Type1,X]),write(' to equip '),printlist([Type,X]).
+
+/*************************************************************************************/
+
+/* Inventory */
+/* Rules */
+/* Untuk print inventory player */
+inventory :- 
+    weapon(Item),
+    printlist(Item),
+    (equip(weapon(Item)),print(' (Equipped)\n');print('\n')),
+    fail.
+inventory :- 
+    armor(Item),
+    printlist(Item),
+    (equip(armor(Item)),print(' (Equipped)\n');print('\n')),
+    fail.
+inventory :- 
+    accessory(Item),
+    printlist(Item),
+    (equip(accessory(Item)),print(' (Equipped)\n');print('\n')),
+    fail.
+inventory :- potion(Type,N),printlist([N,Type,'potion']),print('\n'),fail.
+
+/* Print List */
+printlist([Head|[]]) :- !,print(Head).
+printlist([Head|Tail]) :- print(Head), print(' '),printlist(Tail).
+
+/*************************************************************************************/
+/* Item - item yang dimiliki player akan disimpan dalam bentuk predikat dynamic*/
+
+
---
+++ store.pl
@@ -0,0 +1,53 @@
+/* Store */
+:- include('item.pl').
+
+/* Facts */
+gold(1000).
+
+/* Rules */
+checkGold(X) :- gold(Y),\+ X>Y.
+
+randomItem(Item) :-
+    itemList(I),
+    length(I,X),
+    random(1,X,X1),
+    accessItem(I,X1,Item).
+
+accessItem([Head|_],0,X) :- X=Head,!.
+accessItem([_|Tail],I1,X) :- I is I1-1, accessItem(Tail,I,X).
+
+shop :- /* untuk membuka menu store */
+    print('1. Gacha (100 Gold)\n2. Health Potion (10 Gold)\n'),
+    read(X),
+    (X=gacha, gacha;healthPotion). 
+
+gacha :- /* Untuk random item yang didapatkan player */
+    checkGold(100),!,
+    minusGold(100),
+    randomItem([Type,X]),
+    write('You got...\n'),sleep(0.5),
+    printlist([1|[Type,X]]),print('\n'),
+    checkItem([1|[Type,X]],N),
+    write('Do you want to equip the item ?'),
+    read(Op),
+    insert([N|[Type,X]],Op). 
+gacha :- print('Insufficient amount of gold!').
+
+healthPotion :- /* Untuk menambahkan health potion di inventory player */
+    checkGold(10),
+    potion(health,X),!,
+    minusGold(10),
+    retract(potion(health,X)),
+    X1 is X+1, 
+    assertz(potion(health,X1)).
+healthPotion :- 
+    checkGold(10),!,
+    minusGold(10),
+    assertz(potion(health,1)). 
+healthPotion :- print('Insufficient amount of gold!').
+
+minusGold(X) :- /* Untuk mengurangkan gold player */
+    gold(Y),
+    Y1 is Y-X,
+    retract(gold(Y)),
+    assertz(gold(Y1)). 
