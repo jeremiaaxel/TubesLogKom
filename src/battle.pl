5fb02f9a68f31f83565f36bea2c10928d2d9d17a
jeremiaaxel <jeremiaaxel>
2020-11-21 08:05:27 +0700

    progress battle mech

--- src/battle.pl
+++ src/battle.pl
@@ -1,19 +1,28 @@
 /* Battle */
-dynamic(isEnemyAlive/1).
 
-/* Fakta */
+/* ****** dynamics ****** */
+dynamic(isEnemyAlive/1).
+dynamic(isCharAlive/1).
+dynamic(turns/1).
+dynamic(peluang/1).
+
+/* ****** Fakta ****** */
 isEnemyAlive(0).
 isEnemyAlive(1).
 
 isCharAlive(0).
 isCharAlive(1).
 
-/* Rules */
+/* ****** Rules ****** */
+/* **** found enemy **** */
 foundEnemy :-
     enemy(Name, Level, Type, HP, DP, AP),
     /* \+ enemy_type(boss) */
     asserta(isEnemyAlive(1)),
     asserta(isCharAlive(1)),
+    random(1, 10, P),
+    asserta(peluang(P)),
+    asserta(turns(0)),
     write('You just found an enemy!').
 
 foundBoss :-
@@ -21,14 +30,18 @@ foundBoss :-
     /* enemy_type(boss) */
     asserta(isEnemyAlive(1)),
     asserta(isCharAlive(1)),
+    random(4, 10, P),
+    asserta(peluang(P)),
+    asserta(turns(0)),
     write('Congratulations, you just found a BOSS enemy!').
 
-/* Fight */
+/* **** Fight **** */
+/* harusnya engga ada, auto lawan */
 fight :-
     \+ isEnemyAlive(_),
     write('No enemy').
 
-/* Commentary */
+/* **** Commentary **** */
 attComment :-
     enemy(EnemyName, _, _, EnemyHP, EnemyDP, EnemyAP),
     EnemyHP > 0,
@@ -41,6 +54,8 @@ attComment :-
 attComment :-
     enemy(EnemyName, _, _, EnemyHP, EnemyDP, EnemyAP),
     EnemyHP =< 0,
+    retract(isEnemyAlive(1)),
+    asserta(isEnemyAlive(0)),
     write('You win '), nl,
     write('...'), nl,
     sleep(1),
@@ -56,22 +71,17 @@ enemyAttComment :-
 enemyAttComment :-
     character(CharName, _, _, CharHP, CharDP, CharAP),
     CharHP =< 0,
+    retract(isCharAlive(1)),
     asserta(isCharAlive(0)),
     write('You dead'), nl.
     sleep(1),
     / * ke fail state */
 
-/* Attack */
+/* **** Attack **** */
 charTurn :-
     write('Your turn!'), nl,
     write('...'), nl,
     attack.
-
-/* Player turn but enemy is dead */
-charTurn :-
-    isEnemyAlive(0),
-    write('You win!'), nl,
-    write('...'), nl.
 
 attack :-
     character(_, CharName, _, _, _, _, CharAttack, _, _),
@@ -81,9 +91,42 @@ attack :-
     NewEnemyHP is (EnemyHP-CharAtt),
     asserta(enemy(EnemyName, EnemyType, EnemyLevel, EnemyMaxHP, NewEnemyHP,EnemyDP,EnemyAP)),
     write(CharName), write(' attacks and deals '), write(CharAtt), write(' damage.'), nl,
+    asserta(turns(_+1)),
+    asserta(peluang(_+1)),
     attackComment, !.
 
+/* **** Special Attack **** */
+/* ** Special Attack unavailable ** */
+/* banyak turns < 3 */
+specialAttack :-
+    turns(X),
+    X < 3,
+    write('Special Attack is unavailable '), write(3 - X), write(' turns more.'), nl, 
+    /* gagal special attack, player turn lagi */
+    !.
 
+/* ** Special Attack available ** */
+/* banyak turns >= 3, turns diset ke 0 */
+specialAttack :-
+    turns(X),
+    X >= 3,
+    retract(turns(_)),
+    asserta(turns(0)),
+
+    character(_, CharName, _, _, _, _, CharAttack, _, _),
+    enemy(EnemyName, EnemyLevel, EnemyType, EnemyMaxHP, EnemyHP, EnemyDP, EnemyAP),
+    retract(enemy(EnemyName, EnemyType, EnemyLevel, EnemyMaxHP, EnemyHP, EnemyDP, EnemyAP)),
+    CharAtt is (Attack * 2), 
+    /* specialAttack 2 kali attack biasa (?) */
+    
+    NewEnemyHP is (EnemyHP-CharAtt),
+    asserta(enemy(EnemyName, EnemyType, EnemyLevel, EnemyMaxHP, NewEnemyHP,EnemyDP,EnemyAP)),
+    write(CharName), write(' uses special attack and deals '), write(CharAtt), write(' damage.'), nl,
+    retract(peluang(_)),
+    asserta(peluang(_+1)),
+    attackComment, !.
+
+/* ** Enemy Turn ** */
 enemyTurn :-
     write('Enemy turn!'), nl,
     write('...'),
@@ -98,3 +141,29 @@ enemyAttack :-
     asserta(character(CharName, CharType, CharLevel, CharMaxHP, NewCharHP, CharDP, CharAP)),
     write(EnemyName), write(' attacks and deals '), write(EnemyAP), write(' damage.'), nl,
     enemyAttackComment, !.
+
+
+/* **** Run **** */
+/* Bisa run kalau peluang >= 5 */
+/* Berhasil Run */
+run :- 
+    \+ isRun(_),
+    isEnemyAlive(_),
+    peluang(P),
+    P >= 5,
+    write('You just successfully ran away!'),
+    retract(peluang(P)),
+    retract(isEnemyAlive(_)),
+    retract(enemy(_, _, _, _, _, _, _)),
+    /* ke mana (?) */
+    !.
+
+/* Gagal Run */
+run :- 
+    \+ isRun(_),
+    isEnemyAlive(_),
+    peluang(P),
+    P < 5,
+    write('You failed to run !'),
+    /* ke turn player lagi */
+    ,!.
