fa2c63462bd0c1ef9ee123b0d33646d6369826de
jeremiaaxel <jeremiaaxel>
2020-11-21 07:30:00 +0700

    file main

---
+++ main.pl
@@ -0,0 +1,15 @@
+:- include('src/battle.pl').
+:- include('src/character.pl').
+:- include('src/enemy.pl').
+:- include('src/fail.pl').
+:- include('src/goal.pl').
+:- include('src/item.pl').
+:- include('src/move.pl').
+:- include('src/quest.pl').
+:- include('src/store.pl').
+
+title :-
+    /* welcome msg */
+
+start :-
+    /* command utama untuk start new */
