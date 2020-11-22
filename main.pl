dcd7cd6c78e768d9cf950973e5f4765131508186
jeremiaaxel <jeremiaaxelbachtera@gmail.com>
2020-11-22 12:54:27 +0700

    ulang enemy

--- main.pl
+++ main.pl
@@ -1,15 +1,53 @@
+:- dynamic(init/1).
+
 :- include('src/battle.pl').
 :- include('src/character.pl').
 :- include('src/enemy.pl').
 :- include('src/fail.pl').
 :- include('src/goal.pl').
 :- include('src/item.pl').
+:- include('src/map.pl').
 :- include('src/move.pl').
 :- include('src/quest.pl').
 :- include('src/store.pl').
 
 title :-
     /* welcome msg */
+    write('Welcome to Indomaret. Happy shopping.'), nl,
+    write('Fresh bread please, fresh fruit please.'), nl,
+    write('#######################################################'), nl,
+    write('#               Untitled                              #'), nl,
+    write('# Commands                                            #'), nl,
+    write('# 1. start  : To start to waste your useless life     #'), nl,
+    write('# 2. map    : To show map                             #'), nl,
+    write('# 3. status : To show your status of course           #'), nl,
+    write('# 4. Move commands :                                  #'), nl,
+    write('#            w : move toward north one step           #'), nl,
+    write('#            a : move toward west one step            #'), nl,
+    write('#            s : move toward south one step           #'), nl,
+    write('#            d : move toward east one step            #'), nl,
+    write('# 4. quit   : To quit the game and                    #'), nl,
+    write('#             realize you have assignments to do      #'), nl,
+    write('#######################################################'), nl,
+    
 
 start :-
     /* command utama untuk start new */
+    \+ init(_),
+    asserta(init(1)).
+    title,
+    
+start :-
+    init(_),
+    write('The game has started').
+
+quit :-
+    \+ init(_),
+    write('The game is not started').
+
+quit :-
+    write('Now go! Realize your pitiful life!'), nl,
+    write('Do not forget you have a lot of assignments to be done any time soon!'),
+    /* semua data diretract */
+    retract(init(_)),
+    
--- src/enemy.pl
+++ src/enemy.pl
@@ -1,39 +1,73 @@
+/* Axel */
 /* Enemy */
 
 /* Fakta */
-dynamic(current_hp/1)
+/* dynamic(current_hp/1) */
 
-enemy_type(slime).
-enemy_type(goblin).
-enemy_type(wolf).
-enemy_type(boss).
+/* 
+enemy(ID, Name, Type, Level, HP, DP, AP) 
+*/
 
-enemy_hp(slime, 8000).
-enemy_hp(goblin, 6000).
-enemy_hp(wolf, 5000).
-enemy_hp(boss, 20000).
+/* 1 */
+enemy(11, wolfie, wolf, 1, 1000, 700, 200).
+enemy(12, ingus, slime, 1, 3000, 1000, 100).
+enemy(13, cendol, goblin, 1, 2000, 900, 150).
 
-enemy_def(slime, 8000).
-enemy_def(goblin, 6000).
-enemy_def(wolf, 5000).
-enemy_def(boss, 10000).
+/* 2 */
+enemy(21, kirain_anjing, wolf, 2, 1100, 700, 300).
+enemy(22, slimong, slime, 2, 3200, 1000, 100).
+enemy(23, padatan_klorofil, goblin, 2, 2150, 900, 200).
 
-enemy_att(slime, 500).
-enemy_att(goblin, 600).
-enemy_att(wolf, 800).
-enemy_att(wolf, 600).
+/* 3 */
+enemy(31, mountain_wolf, wolf, 3, 1100, 700, 300).
+enemy(32, , slime, 3, 3200, 1000, 100).
+enemy(33, cendol, goblin, 3, 2150, 900, 200).
+
+/* 4 */
+enemy(41, kirain_anjing, wolf, 4, 1100, 700, 300).
+enemy(42, slimong, slime, 4, 3200, 1000, 100).
+enemy(43, cendol, goblin, 4, 2150, 900, 200).
+
+/* 5 */
+enemy(51, kirain_anjing, wolf, 5, 1100, 700, 300).
+enemy(52, slimong, slime, 5, 3200, 1000, 100).
+enemy(53, cendol, goblin, 5, 2150, 900, 200).
+
+/* 6 */
+enemy(61, kirain_anjing, wolf, 6, 1100, 700, 300).
+enemy(62, slimong, slime, 6, 3200, 1000, 100).
+enemy(63, cendol, goblin, 6, 2150, 900, 200).
+
+/* 7 */
+enemy(71, kirain_anjing, wolf, 7, 1100, 700, 300).
+enemy(72, slimong, slime, 7, 3200, 1000, 100).
+enemy(73, cendol, goblin, 7, 2150, 900, 200).
+
+/* 8 */
+enemy(81, kirain_anjing, wolf, 8, 1100, 700, 300).
+enemy(82, slimong, slime, 8, 3200, 1000, 100).
+enemy(83, cendol, goblin, 8, 2150, 900, 200).
+
+/* 9 */
+enemy(91, kirain_anjing, wolf, 9, 1100, 700, 300).
+enemy(92, slimong, slime, 9, 3200, 1000, 100).
+enemy(93, cendol, goblin, 9, 2150, 900, 200).
+
+/* 10 */
+enemy(101, kirain_anjing, wolf, 10, 1100, 700, 300).
+enemy(102, slimong, slime, 10, 3200, 1000, 100).
+enemy(103, cendol, goblin, 10, 2150, 900, 200).
+
+/* bosses */
+enemy(34, king_meong, boss, 3, 10000, 2000, 500).
+enemy(54, sir_yon, boss, 5, 299314, 5000, 690).
+enemy(74, eternity_single, boss, 7, 1111111, 8011, 769).
+enemy(94, praprak_logkom, boss, 9, 1423599, 9500, 1000).
+enemy(104, tubes_logkom, boss, 10, 18235959, 9999, 999).
+
+
 
 /* Current HP */
 /* current_hp([10]). */
 
 /* Rules */
-enemy(Name, Type, Level, MaxHP, HP, DP, AP) :-
-    enemy_type(Type),
-    enemy_hp(Type, Base_hp),
-    enemy_def(Type, Base_def),
-    enemy_att(Type, Base_att),
-    MaxHP is Base_hp * Level,
-    HP is Base_hp * Level,
-    DP is Base_def * Level,
-    AP is Base_att * Level,
-    !.
