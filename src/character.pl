7b680a4d3487d15074706aa3636e0a3e22b047e5
jeremiaaxel <jeremiaaxel@LAPTOP-8L6KICJ0.localdomain>
2020-11-24 19:08:50 +0700

    add status and minor fix movement

--- src/character.pl
+++ src/character.pl
@@ -1,7 +1,7 @@
 :- dynamic(character/7).
 
 /* Job: swordsman, archer, sorcerer */
-/* character(Nama, Job, Level, MaxHP, HP, DP, AP) */
+/* character(Nama, Job, Level, MaxHP, HP, DP, AP, Exp) */
 character(_, swordsman, _, 3000, 3000, 2000, 300, _).
 character(_, archer, _, 3000, 3000, 1500, 400, _).
 character(_, sorcerer, _, 2500, 2500, 1000, 450, _).
