c533ab83ba614404737db79317536bae43360ece
epata <epatatuah180901@gmail.com>
2020-11-25 16:44:17 +0700

    quest and goal update

--- src/goal.pl
+++ src/goal.pl
@@ -8,9 +8,15 @@ winFinal(boss) :- enemy(_,boss,_,_,0,_,_).
 
 goal :- winFinal(boss),
         !,
+        write('██╗   ██╗ ██████╗ ██╗   ██╗    ██╗    ██╗ ██████╗ ███╗   ██╗'),nl,
+        write('╚██╗ ██╔╝██╔═══██╗██║   ██║    ██║    ██║██╔═══██╗████╗  ██║'),nl,
+        write(' ╚████╔╝ ██║   ██║██║   ██║    ██║ █╗ ██║██║   ██║██╔██╗ ██║'),nl,
+        write('  ╚██╔╝  ██║   ██║██║   ██║    ██║███╗██║██║   ██║██║╚██╗██║'),nl,
+        write('   ██║   ╚██████╔╝╚██████╔╝    ╚███╔███╔╝╚██████╔╝██║ ╚████║'),nl,
+        write('   ╚═╝    ╚═════╝  ╚═════╝      ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═══╝'),nl,
         write('Your soul are now attached with the Dovnik, the son of Mathevik'),nl,
-        write('Shall began, Dragon Slayer...'),nl,
-        write('You are a hero of Wangardium.').
+        write('              ...Shall began, Dragon Slayer...'),nl,
+        write('              You are a hero of Willy Wangky').
 
 /* Pemain kalah melawan bos */
 goal :- character(_,_,_,_,0,_,_),!,
--- src/quest.pl
+++ src/quest.pl
@@ -1,31 +1,82 @@
 /* QUEST */
-/*:- dynamic(quest/3) quest(slime,goblin,wolf)*/
 :- dynamic(questOnGoing/1).
-:- dynamic(quest/3).
+:- dynamic(quest1/3).
+:- dynamic(quest2/3).
+:- dynamic(quest3/3).
 :- dynamic(expGain/1).
-:- dynamic(options/1).
 
 /*Rules*/
 /*mekanisme quest*/
-/* QUEST 1 */
-foundQuest :- posPlayer(X,Y),quest1(X,Y),\+(questOnGoing(1)),asserta(quest(1,1,1)),
-    write('Veelfdir: Ohh dear Willy Wangky\'s grandson, can you help me for something?'),nl,
-    write('Choose your dialogue option with command \'1\', \'2\', or \'3\'.'),nl,
-    write('Options: i.   Alright. What can I do?'),nl,
-    write('         ii.  I\'m in a hurry. Just do it fast.'),nl,
-    write('         iii. Give me some \'wares\' first before that \'helping\' thing.'),
-    asserta(options(1)), asserta(questOnGoing(1)).
 
-foundQuest :- posPlayer(X,Y),quest2(X,Y),\+(questOnGoing(1)),asserta(quest(2,3,2)),
+/* QUEST 1 */
+foundQuest :- posPlayer(X,Y),quest1(X,Y),\+(questOnGoing(1)),asserta(quest1(1,1,1)),
+    write('Veelfdir: Ohh dear Willy Wangky\'s grandson, can you help me for something?'),nl,sleep(0.5),
+    write('[1] Alright. What can I do?'),nl,
+    write('[2] I\'m in a hurry. Just do it fast.'),nl,
+    write('[3] Give me some \'wares\' first before that \'helping\' thing.'),nl,
+    repeat,
+    write('Your option: '),read(OO),
+    (
+        (OO\=1);(OO\=2);(OO\=3) ->
+            fail;
+            !
+    ),
+    ((OO=1,nl,write('Veelfdir: My bianglala has been burned by those filthy monsters! Please, get rid of them all'));
+    (OO=2,nl,write('Veelfdir: My farm has been burned by those filthy monsters! Please, get rid of them all'));
+    (OO=3,nl,write('Veelfdir: And I oop...What happened that you make the darkness led behind your cruel words, sir?'),nl,
+    write('          I have no wares left. My farm has been burned by those filthy monsters!'))),nl,sleep(0.5),
+    write('[1] Alright. I\'ll show them this great Wangky Grandson wkwk.'),nl,
+    write('[2] My condolences. I will show them no mercy.'),nl,
+    write('[3] Ahh they are just a bunch of dog yet you can\'t get rid of them. Alright alright.'),nl,
+    repeat,
+    write('Your option: '),read(PP),
+    (
+        (PP\=1);(PP\=2);(PP\=3) ->
+            fail;
+            !
+    ),
+    ((PP=1,nl,write('Veelfdir: Thank you, sir.'));
+    (PP=2,nl,write('Veelfdir: Thank you, dear sir. I hope you could came back with no dirty rags.'));
+    (PP=3,nl,write('Veelfdir: *silent*'))),nl,sleep(0.5),
+    write('##############QUEST 1##############'),nl,
+    write('Kill 1 slime, 1 goblin, and 1 wolf.'),
+    asserta(questOnGoing(1)).
+
+/* QUEST 2*/
+foundQuest :- posPlayer(X,Y),quest2(X,Y),\+(questOnGoing(1)),asserta(quest2(2,3,2)),
     write('Laborers  : This is the time! This theme park should came back to our hands!'),nl,
     write('          Hours by hours, carried by the courage and sacrifice of our fellows!'),nl,
-    write('          The sun are watching. The spirits of our customers are stirring.'),nl,
-    write('          Fight! Fight! Fight! for the landlords of Willy Wangky!'),nl,
-    write('Options: i.  Hello laborers, where are you going?'),nl,
-    write('         ii. *pretend not to see*'),nl,
-    asserta(options(1)),asserta(questOnGoing(1)).
+    write('          The sun are watching. The spirits of our customers are stirring.'),nl,sleep(0.5),
+    write('          Fight! Fight! Fight! for the landlords of Willy Wangky!'),nl,sleep(0.5),
+    write('[1] Hello laborers, where are you going?'),nl,
+    write('[2] *pretend not to see*'),nl,
+    repeat,
+    write('Your option: '),read(QQ),
+    (
+        (QQ\=1),(QQ\=2) ->
+            fail;
+            !
+    ),
+    ((QQ=1,nl,write('Laborers  : We are trying to accomplish your grandfather, Willy Wangky\'s goal,'),nl,
+    write('            to remove impurities in this theme park. Will you join us, dear sir?'));
+    (QQ=2,nl,write('Laborers  : Sunrise above, Willy Wangky\'s grandson. Are you interested to join our battle?'))),nl,sleep(0.5),
+    write('[1] I will gladly join our countrymen\'s battle.'),nl,
+    write('[2] Alright alright'),nl,
+    repeat,
+    write('Your option: '),read(RR),
+    (
+        (RR\=1),(RR\=2) ->
+            fail;
+            !
+    ),
+    ((RR=1,nl,write('Laborers  : Let\'s go with us then, sir.'));
+    (RR=2,nl,write('Laborers  : Sounds lazy... Anyway, let\'s go.'))),nl,sleep(0.5),
+    write('##############QUEST 2##############'),nl,
+    write('Kill 2 slimes, 3 goblins, 2 wolves'),
+    asserta(questOnGoing(1)).
 
-foundQuest :- posPlayer(X,Y),quest3(X,Y),\+(questOnGoing(1)),asserta(quest(5,6,4)),
+/* QUEST 3 */
+foundQuest :- posPlayer(X,Y),quest3(X,Y),\+(questOnGoing(1)),asserta(quest3(5,6,4)),
     write('  ` : | | | |:  ||  :     `  :  |  |+|: | : : :|   .        `              .'),nl,
     write('      ` : | :|  ||  |:  :    `  |  | :| : | : |:   |  .                    :'),nl,
     write('         .\' \':  ||  |:  |  \'       ` || | : | |: : |   .  `           .   :.'),nl,
@@ -51,92 +102,93 @@ foundQuest :- posPlayer(X,Y),quest3(X,Y),\+(questOnGoing(1)),asserta(quest(5,6,4
     write('Dovahngard : hanaca... raka... datasawala... padhajaya... nyamagabat... hanga...'),nl,sleep(0.5),
     write('Dovahngard : hanaca... raka...........'),nl,sleep(0.5),
     write('Dovahngard : ....Oh dear.... Those visions are true......I see the light of Wangkygard inside your eyes...'),nl,sleep(0.5),
-    write('Options: i.  What do you mean by that, old man?'),nl,
-    write('         ii. Wangkygard...um what?'),
-    asserta(options(1)),asserta(questOnGoing(1)).
+    write('[1] What do you mean by that, old man?'),nl,
+    write('[2] Wangkygard...um what?'),nl,
+    repeat,
+    write('Your option: '),read(SS),
+    (
+        (SS\=1),(SS\=2) ->
+            fail;
+            !
+    ),
+    ((SS=1,nl,write('Dovahngard : The prophecy...hanaca... has said that will come a man... whom descendant of the great Willy Wangky...'),nl,
+    write('             ...to break all the indecency by the darkness that has been corrupting the land of Willy Wangky...'),nl,
+    write('Dovahngard : ...I believe you are the one who could end the misery on this land... sir...'));
+    (SS=2,nl,write('Dovahngard : Wangkygard.....The soul who once own this land...Now he appears, the descendant of the great Willy Wangky...'),nl,
+    write('           : ...to break all the indecency by the darkness that has been corrupting the land of Willy Wangky...'),nl,sleep(0.5),
+    write('Dovahngard : ...I believe you are the one who could end the misery on this land... sir...'))),nl,sleep(0.5),
+    write('[1] What should I do? I mean, all these monsters just appear with no ending sequence, old man.'),nl,
+    repeat,
+    write('Your option: '), read(TT),
+    (
+        TT \= 1 ->
+            fail;
+            !
+    ),
+    (TT=1,nl,write('Dovahngard : ...In the mean time... this old earthling just requests you to clean my Histeria from those impurities...'),nl,sleep(0.5),
+    write('             ...but those things will never end until you.....end the biggest impurities laid beneath this land....'),nl,
+    write('             ...God give me a hope to this land as the sunrise could be seen again...')),nl,nl,sleep(0.5),
+    write('##############QUEST 3##############'),nl,
+    write('Kill 5 slimes, 6 goblins, and 4 wolves.'),
+    asserta(questOnGoing(1)).
 
 /* Jika masih ada quest yang berjalan */
 foundQuest :- posPlayer(X,Y),(quest1(X,Y);quest2(X,Y);quest3(X,Y)),questOnGoing(1),
     write('You are still in a quest, mortal. Finish it up first.'),!.
 
+/* Mekanisme exp Quest */
+expGoldQuest1 :- character(Name, Job, CharLevel, MaxHP, HP, DP, AP, Exp),
+    quest1(0,0,0), quest1Finish, Exp2 is Exp+100, plusGold(250),
+    retract(character(_, _, _, _, _, _, _, _)),
+    asserta(character(Name, Job, CharLevel, MaxHP, HP, DP, AP, Exp2)),
+    isLevelUp,
+    !.
+expGoldQuest2 :- character(Name, Job, CharLevel, MaxHP, HP, DP, AP, Exp),
+    quest2(0,0,0), quest2Finish, Exp2 is Exp+300, plusGold(500),
+    retract(character(_, _, _, _, _, _, _, _)),
+    asserta(character(Name, Job, CharLevel, MaxHP, HP, DP, AP, Exp2)),
+    isLevelUp,
+    !.
+expGoldQuest3 :- character(Name, Job, CharLevel, MaxHP, HP, DP, AP, Exp),
+    quest3(0,0,0), quest3Finish, Exp2 is Exp+500, plusGold(800),
+    retract(character(_, _, _, _, _, _, _, _)),
+    asserta(character(Name, Job, CharLevel, MaxHP, HP, DP, AP, Exp2)),
+    isLevelUp,
+    !. 
 
-opt :- posPlayer(X,Y),quest1(X,Y),questOnGoing(1),quest(1,1,1),options(1),
-    write('Options: i.   Alright. I\'ll show them this great Wangky Grandson wkwk.'),nl,
-    write('         ii.  My condolences. I will show them no mercy.'),nl,
-    write('         iii. Ahh they are just a bunch of dog yet you can\'t get rid of them. Alright alright.'),
-    retract(options(1)),asserta(options(2)).
+/* Jika quest selesai */
+quest1Finish :- write('You gain 100 exp!'),retract(quest1(0,0,0)).
+quest2Finish :- write('You gain 300 exp!'),retract(quest2(0,0,0)).
+quest3Finish :- write('You gain 500 exp!'),retract(quest3(0,0,0)).
 
-opt :- posPlayer(X,Y),quest2(X,Y),questOnGoing(1),quest(2,3,2),options(1),
-    write('Choose your dialogue option with command \'1\', \'2\'.'),nl,
-    write('Options: i.  I will gladly join our countrymen\'s battle.'),nl,
-    write('         ii. Alright alright'),
-    retract(options(1)),asserta(options(2)).
+/* kerangka reward quest yg bakal ditambahin di battle.py sambil menunggu yang fix */
+/* 
+winX :- (enemyInFight(_,_,_,slime,_,_,_,_)).
+winY :- (enemyInFight(_,_,_,goblin,_,_,_,_)).
+winZ :- (enemyInFight(_,_,_,wolf,_,_,_,_)).
 
-i :- quest(1,1,1), options(1),!,
-    write('Veelfdir: My bianglala has been burned by those filthy monsters! Please, get rid of them all'), opt.
-i :- quest(1,1,1),options(2),!,
-    write('Veelfdir: Thank you, sir.'),nl,nl,
-    write('##############QUEST 1##############'),nl,
-    write('Kill 1 slime, 1 goblin, and 1 wolf.'),
-    retract(options(2)).
-i :- quest(2,3,2),options(1),!,
-    write('Laborers  : We are trying to accomplish your grandfather, Willy Wangky\'s goal,'),nl,
-    write('            to remove the impurities in this theme park. Will you join us, dear sir?'),opt.
-i :- quest(2,3,2),options(2),!,
-    write('Laborers  : Let\'s go with us then, sir.'),nl,
-    write('##############QUEST 2##############'),nl,
-    write('Kill 2 slimes, 3 goblins, 2 wolves'),
-    retract(options(2)).
-i :- quest(5,6,4),options(1),!,
-    write('Dovahngard : The prophecy...hanaca... has said that will come a man... whom descendant of the great Willy Wangky...'),nl,
-    write('             ...to break all the indecency by the darkness that has been corrupting the land of Willy Wangky...'),nl,
-    write('Dovahngard : ...I believe you are the one who could end the misery on this land... sir...'),nl,sleep(0.5),
-    write('Options: i. What should I do? I mean, all these monsters just appear with no ending sequence, old man.'),
-    retract(options(1)),asserta(options(2)).
-i :- quest(5,6,4),options(2),!,
-    write('Dovahngard : ...In the mean time... this old earthling just requests you to clean my Histeria from those impurities...'),nl,sleep(0.5),
-    write('             ...but those things will never end until you.....end the biggest impurities laid beneath this land....'),nl,
-    write('             ...God give me a hope to this land as the sunrise could be seen again...'),nl,sleep(0.5),
-    write('##############QUEST 3##############'),nl,
-    write('Kill 5 slimes, 6 goblins, and 4 wolves.').
+winQuest1 :- ((winX, XNew is X-1, retract(quest1(X,Y,Z)),asserta(quest1(XNew,Y,Z)));
+(winY, YNew is Y-1, retract(quest1(X,Y,Z)),asserta(quest1(X,YNew,Z)));
+(winZ, ZNew is Z-1, retract(quest1(X,Y,Z)),asserta(quest1(X,Y,ZNew)))).  
 
-ii :- quest(1,1,1), options(1),!,
-    write('Veelfdir: My farm has been burned by those filthy monsters! Please, get rid of them all'), opt.
-ii :- quest(1,1,1),options(2),!,
-    write('Veelfdir: Thank you, dear sir. I hope you could came back with no dirty rags.'),nl,nl,
-    write('##############QUEST 1##############'),nl,
-    write('Kill 1 slime, 1 goblin, and 1 wolf.'),
-    retract(options(2)).
-ii :- quest(2,3,2),options(1),!,
-    write('Laborers  : Sunrise above, Willy Wangky\'s grandson. Are you interested to join our battle?'),opt.
-ii :- quest(2,3,2),options(2),!,
-    write('Laborers  : Sounds lazy... Anyway, let\'s go.'),nl,
-    write('##############QUEST 2##############'),nl,
-    write('Kill 2 slimes, 3 goblins, and 2 wolves'),
-    retract(options(2)).
-ii :- quest(5,6,4),options(1),!,
-    write('Dovahngard : Wangkygard.....The soul who once own this land...Now he appears, the descendant of the great Willy Wangky...'),nl,
-    write('           : ...to break all the indecency by the darkness that has been corrupting the land of Willy Wangky...'),nl,sleep(0.5),
-    write('Dovahngard : ...I believe you are the one who could end the misery on this land... sir...'),nl,sleep(0.5),
-    write('Options: [1] What should I do? I mean, all these monsters just appear with no ending sequence, old man.'),
-    retract(options(1)),asserta(options(2)).
+winQuest2 :- ((winX, XNew is X-1, retract(quest2(X,Y,Z)),asserta(quest2(XNew,Y,Z)));
+(winY, YNew is Y-1, retract(quest2(X,Y,Z)),asserta(quest2(X,YNew,Z)));
+(winZ, ZNew is Z-1, retract(quest2(X,Y,Z)),asserta(quest2(X,Y,ZNew)))). 
 
-iii :- quest(1,1,1), options(1),!,
-    write('Veelfdir: And I oop...What happened that you make the darkness led behind your cruel words, sir?'),nl,
-    write('          I have no wares left. My farm has been burned by those filthy monsters!'), opt.
-iii :- quest(1,1,1),options(2),!,
-    write('Veelfdir: *silent*'),nl,nl,
-    write('##############QUEST 1##############'),nl,
-    write('Kill 1 slime, 1 goblin, and 1 wolf.'),
-    retract(options(2)).
+winQuest3 :- ((winX, XNew is X-1, retract(quest3(X,Y,Z)),asserta(quest3(XNew,Y,Z)));
+(winY, YNew is Y-1, retract(quest3(X,Y,Z)),asserta(quest3(X,YNew,Z)));
+(winZ, ZNew is Z-1, retract(quest3(X,Y,Z)),asserta(quest3(X,Y,ZNew)))). 
 
+isInQuest(1) :- quest1(X,Y,Z);quest2(X,Y,Z);quest3(X,Y,Z). 
+isInQuest(0) :- \+quest1(X,Y,Z),\+quest2(X,Y,Z),\+quest3(X,Y,Z).
 
-questFinish :- win(2,2,2),
-    asserta(expGain(100)),
-    write('You gain 100 exp!'),!.
-questFinish :- win(4,4,4),
-    asserta(expGain(200)),
-    write('You gain 200 exp!'),!.
-questFinish :- win(6,6,6),
-    asserta(expGain(400)),
-    write('You gain 400 exp!'),!.
+isQuestFinish :- expGoldQuest1 ; expGoldQuest2 ; expGoldQuest3.
+
+Di fungsi attComment yang kondisi menang,
+if IsInQuest(1) then
+    (quest1(X,Y,Z),winQuest1);
+    (quest2(X,Y,Z),winQuest2);
+    (quest3(X,Y,Z),winQuest3),
+    isQuestFinish.
+*/
+
