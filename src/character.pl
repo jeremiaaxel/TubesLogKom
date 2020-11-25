6f7df1845b14342f9d598fdcd70fa989c6275d3e
fluffyotter <blcktmyie123@gmail.com>
2020-11-25 18:49:56 +0700

    minor fix

--- src/character.pl
+++ src/character.pl
@@ -1,7 +1,5 @@
-:- dynamic(character/8).
-
 /* Job: swordsman, archer, sorcerer */
 /* character(Nama, Job, Level, CharLevel, MaxHP, HP, DP, AP, Exp) */
-character(_, swordsman, _, _, 3000, 2000, 300, _).
-character(_, archer, _, _, 3000, 1500, 400, _).
-character(_, sorcerer, _, _, 2500, 2500, 450, _).
+character(_, swordsman, _, _, 3000, _, 2000, 300, _).
+character(_, archer, _, _, 3000, _, 1500, 400, _).
+character(_, sorcerer, _, _, 2500, _, 2500, 450, _).
