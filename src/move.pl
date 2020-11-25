9736b0e5301bbd97c505235a99259125a833e8b6
epata <epatatuah180901@gmail.com>
2020-11-25 11:50:12 +0700

    move hampir fix banget, update guest yg masih onprogress (kalau aja mau liat-liat)

--- src/move.pl
+++ src/move.pl
@@ -1,18 +1,26 @@
 /* Exploration Mechanism */
-
+:- dynamic(newMove/1).
 /* rules */
 notQuest(P,Q) :- \+quest1(P,Q),\+quest2(P,Q),\+quest3(P,Q).
-yesQuest(P,Q) :- quest1(P,Q);quest2(P,Q);quest3(P,Q).
+yesQuest(P,Q) :- (quest1(P,Q);quest2(P,Q);quest3(P,Q)).
 
 /* randomize enemy */
-enemyRandomW(0) :- write('You move north.').
-enemyRandomW(1) :- foundEnemy.
-enemyRandomA(0) :- write('You move west.').
-enemyRandomA(1) :- foundEnemy.
-enemyRandomS(0) :- write('You move south.').
-enemyRandomS(1) :- foundEnemy.
-enemyRandomD(0) :- write('You move east.').
-enemyRandomD(1) :- foundEnemy.
+enemyRandomW(0) :- retract(newMove(1)),asserta(newMove(0)),
+    write('You move north.').
+enemyRandomW(1) :- retract(newMove(1)),asserta(newMove(0)),
+    foundEnemy.
+enemyRandomA(0) :- retract(newMove(1)),asserta(newMove(0)),
+    write('You move west.').
+enemyRandomA(1) :- retract(newMove(1)),asserta(newMove(0)),
+    foundEnemy.
+enemyRandomS(0) :- retract(newMove(1)),asserta(newMove(0)),
+    write('You move south.').
+enemyRandomS(1) :- retract(newMove(1)),asserta(newMove(0)),
+    foundEnemy.
+enemyRandomD(0) :- retract(newMove(1)),asserta(newMove(0)),
+    write('You move east.').
+enemyRandomD(1) :- retract(newMove(1)),asserta(newMove(0)),
+    foundEnemy.
 
 /*pemain berpindah 1 tile ke atas dengan mekanisme kemunculan musuh yang random*/
 w :- init(_),
@@ -22,49 +30,46 @@ w :- init(_),
     \+wall(X,Z),
     retract(posPlayer(X,Y)),
     asserta(posPlayer(X,Z)),
+    retract(newMove(0)),
+    asserta(newMove(1)),
     \+dungeon(X,Z),notQuest(X,Z),\+store(X,Z),
-    random(0,2,E), enemyRandomW(E),!.
+    enemyRandomW(0),!.
+
+/* Jika nabrak pagar */
+w :- newMove(0),
+    init(_),
+    fighting(0),
+    posPlayer(X,Y),
+    Z is Y-1,
+    wall(X,Z),
+    write('You hit a wall. I began to lose faith in you, mortal.'),!.
 
 /* Jika berada di tile bos*/
 w :- init(_),
     fighting(0),
     posPlayer(X,Y),
-    Z is Y-1,
-    \+wall(X,Z),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(X,Z)),
-    dungeon(X,Z),
+    dungeon(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
     foundBoss,!.
 
 /* Jika menemukan quest */
 w :- init(_),
     fighting(0),
     posPlayer(X,Y),
-    Z is Y-1,
-    \+wall(X,Z),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(X,Z)),
-    yesQuest(X,Z),
+    yesQuest(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
     foundQuest,!.
 
 /* Jika berada di store */
 w :- init(_),
     fighting(0),
     posPlayer(X,Y),
-    Z is Y-1,
-    \+wall(X,Z),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(X,Z)),
-    store(X,Z),
+    store(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
     write('Hello, sir. Welcome to my store! Here, take a look'),!.
-
-/* Jika nabrak pagar */
-w :- init(_),
-    fighting(0),
-    posPlayer(X,Y),
-    Z is Y-1,
-    wall(X,Z),
-    write('You hit a wall. I began to lose faith in you, mortal.'),!.
 
 /* Jika game belum mulai */
 w :- \+init(_),
@@ -84,49 +89,46 @@ a :- init(_),
     \+wall(Z,Y),
     retract(posPlayer(X,Y)),
     asserta(posPlayer(Z,Y)),
+    retract(newMove(0)),
+    asserta(newMove(1)),
     \+dungeon(Z,Y),notQuest(Z,Y),\+store(Z,Y),
-    random(0,2,E), enemyRandomA(E),!.
+    enemyRandomA(0),!.
+
+/* jika menabrak pagar */
+a :-newMove(0),
+    init(_),
+    fighting(0),
+    posPlayer(X,Y),
+    Z is X-1,
+    wall(Z,Y),
+    write('You hit a wall. I began to lose faith in you, mortal.'),!.
 
 /* Jika berada di tile bos */
 a :- init(_),
     fighting(0),
     posPlayer(X,Y),
-    Z is X-1,
-    \+wall(Z,Y),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(Z,Y)),
-    dungeon(Z,Y),
+    dungeon(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
     foundBoss,!.
 
 /* Jika menemukan quest */
 a :- init(_),
     fighting(0),
     posPlayer(X,Y),
-    Z is X-1,
-    \+wall(Z,Y),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(Z,Y)),
-    yesQuest(Z,Y),
+    yesQuest(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
     foundQuest,!.
 
 /* Jika berada di tile store */
 a :- init(_),
     fighting(0),
     posPlayer(X,Y),
-    Z is X-1,
-    \+wall(Z,Y),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(Z,Y)),
-    store(Z,Y),
+    store(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
     write('Hello, sir. Welcome to my store! Here, take a look.'),!.
-
-/* jika menabrak pagar */
-a :- init(_),
-    fighting(0),
-    posPlayer(X,Y),
-    Z is X-1,
-    wall(Z,Y),
-    write('You hit a wall. I began to lose faith in you, mortal.'),!.
 
 /* Jika game belum dimulai */
 a :- \+init(_),
@@ -147,49 +149,46 @@ s :- init(_),
     \+wall(X,Z),
     retract(posPlayer(X,Y)),
     asserta(posPlayer(X,Z)),
+    retract(newMove(0)),
+    asserta(newMove(1)),
     \+dungeon(X,Z),notQuest(X,Z),\+store(X,Z),
-    random(0,2,E),enemyRandomS(E),!.
-
-/* Jika ketemu bos */
-s :- init(_),
-    fighting(0),
-    posPlayer(X,Y),
-    Z is Y+1,
-    \+wall(X,Z),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(X,Z)),
-    dungeon(X,Z),
-    foundBoss,!.
-
-/* Jika menemukan quest */
-s :- init(_),
-    fighting(0),
-    posPlayer(X,Y),
-    Z is Y+1,
-    \+wall(X,Z),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(X,Z)),
-    yesQuest(X,Z),
-    foundQuest,!.
-
-/* Jika berada di tile store */
-s :- init(_),
-    fighting(0),
-    posPlayer(X,Y),
-    Z is Y+1,
-    \+wall(X,Z),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(X,Z)),
-    store(X,Z),
-    write('Hello, sir. Welcome to my store! Here, take a look.'),!.
+    enemyRandomS(0),!.
 
 /* Jika menabrak pagar */
-s :- init(_),
+s :-newMove(0),
+    init(_),
     fighting(0),
     posPlayer(X,Y),
     Z is Y+1,
     wall(X,Z),
     write('You hit a wall. I began to lose faith in you, mortal.'),!.
+
+/* Jika ketemu bos */
+s :-init(_),
+    fighting(0),
+    posPlayer(X,Y),
+    dungeon(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
+    foundBoss,!.
+
+/* Jika menemukan quest */
+s :-init(_),
+    fighting(0),
+    posPlayer(X,Y),
+    yesQuest(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
+    foundQuest,!.
+
+/* Jika berada di tile store */
+s :-init(_),
+    fighting(0),
+    posPlayer(X,Y),
+    store(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
+    write('Hello, sir. Welcome to my store! Here, take a look.'),!.
 
 /* Jika game belum dimulai */
 s :- \+init(_),
@@ -210,49 +209,46 @@ d :- init(_),
     \+wall(Z,Y),
     retract(posPlayer(X,Y)),
     asserta(posPlayer(Z,Y)),
+    retract(newMove(0)),
+    asserta(newMove(1)),
     \+dungeon(Z,Y),notQuest(Z,Y),\+store(Z,Y),
-    random(0,2,E), enemyRandomD(E),!.
+    enemyRandomD(0),!.
 
-/* Jika berada di tile bos */
-d :- init(_),
+/* Jika menabrak pagar */
+d :-newMove(0),
+    init(_),
     fighting(0),
     posPlayer(X,Y),
     Z is X+1,
-    \+wall(Z,Y),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(Z,Y)),
-    dungeon(Z,Y),
+    wall(Z,Y),
+    write('You hit a wall. I began to lose faith in you, mortal.'),!.
+
+/* Jika berada di tile bos */
+d :-init(_),
+    fighting(0),
+    posPlayer(X,Y),
+    dungeon(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
     foundBoss,!.
 
 /* Jika menemukan quest */
 d :- init(_),
     fighting(0),
     posPlayer(X,Y),
-    Z is X+1,
-    \+wall(Z,Y),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(Z,Y)),
-    yesQuest(Z,Y),
+    yesQuest(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
     foundQuest,!.
 
 /* Jika berada di tile store */
 d :- init(_),
     fighting(0),
     posPlayer(X,Y),
-    Z is X+1,
-    \+wall(Z,Y),
-    retract(posPlayer(X,Y)),
-    asserta(posPlayer(Z,Y)),
-    store(Z,Y),
+    store(X,Y),
+    retract(newMove(1)),
+    asserta(newMove(0)),
     write('Hello, sir. Welcome to my store! Here, take a look.'),!.
-
-/* Jika menabrak pagar */
-d :- init(_),
-    fighting(0),
-    posPlayer(X,Y),
-    Z is X+1,
-    wall(Z,Y),
-    write('You hit a wall. I began to lose faith in you, mortal.'),!.
 
 /* Jika game belum dimulai */
 d :- \+init(_),
--- src/quest.pl
+++ src/quest.pl
@@ -3,41 +3,132 @@
 :- dynamic(questOnGoing/1).
 :- dynamic(quest/3).
 :- dynamic(expGain/1).
-
-/*Fact*/
-win(2,2,2).
-win(3,3,3).
-win(4,4,4).
+:- dynamic(options/1).
 
 /*Rules*/
 /*mekanisme quest*/
-foundQuest :- quest1(_,_),
-    \+(questOnGoing(true)),
-    asserta(quest(1,1,1)),
-    write('.....shhhhhhhhhhhhhhhhhhhh....'),nl,
-    write('Sir, my name is Ivankov, and'),nl,
-    write('My farm has been burned by these brutal monsters! Please, help me.'), nl,nl,
-    write('You should kill 1 slimes, 1 goblins, and 1 wolfs'),
-    asserta(questOnGoing(true)),!.
-foundQuest :- quest2(_,_),
-    \+(questOnGoing(true)),
-    asserta(quest(2,3,2)),
-    write('My dear friend! I need your help here.'),nl,
-    write('This village has been terrorized! My friend lose 2 children last night.'),nl,
-    write('I hope my villagers make a smiley face, again.'),nl,nl,
-    write('You should kill 2 slimes, 3 goblins, and 2 wolfs'),
-    asserta(questOnGoing(true)),!.
-foundQuest :- quest3(_,_),
-    \+(questOnGoing(true)),
-    asserta(quest(2,3,4)),
-    write('Uh..oh..HELP'),nl,nl,
-    write('Kill 2 slimes, 3 goblins, and 4 wolfs!'),
-    asserta(questOnGoing(true)),!.
+/* QUEST 1 */
+foundQuest :- posPlayer(X,Y),quest1(X,Y),\+(questOnGoing(1)),asserta(quest(1,1,1)),
+    write('Veelfdir: Ohh dear Willy Wangky\'s grandson, can you help me for something?'),nl,
+    write('Choose your dialogue option with command \'1\', \'2\', or \'3\'.'),nl,
+    write('Options: i.   Alright. What can I do?'),nl,
+    write('         ii.  I\'m in a hurry. Just do it fast.'),nl,
+    write('         iii. Give me some \'wares\' first before that \'helping\' thing.'),
+    asserta(options(1)), asserta(questOnGoing(1)).
 
-/*masih ada quest yang berjalan */
-foundQuest :- questOnGoing(true),
-    write('You are still in a quest, mortal. Are you trying multitask on them?'),!.
+foundQuest :- posPlayer(X,Y),quest2(X,Y),\+(questOnGoing(1)),asserta(quest(2,3,2)),
+    write('Laborers  : This is the time! This theme park should came back to our hands!'),nl,
+    write('          Hours by hours, carried by the courage and sacrifice of our fellows!'),nl,
+    write('          The sun are watching. The spirits of our customers are stirring.'),nl,
+    write('          Fight! Fight! Fight! for the landlords of Willy Wangky!'),nl,
+    write('Options: i.  Hello laborers, where are you going?'),nl,
+    write('         ii. *pretend not to see*'),nl,
+    asserta(options(1)),asserta(questOnGoing(1)).
 
+foundQuest :- posPlayer(X,Y),quest3(X,Y),\+(questOnGoing(1)),asserta(quest(5,6,4)),
+    write('  ` : | | | |:  ||  :     `  :  |  |+|: | : : :|   .        `              .'),nl,
+    write('      ` : | :|  ||  |:  :    `  |  | :| : | : |:   |  .                    :'),nl,
+    write('         .\' \':  ||  |:  |  \'       ` || | : | |: : |   .  `           .   :.'),nl,
+    write('                `\'  ||  |  \' |   *    ` : | | :| |*|  :   :               :|'),nl,
+    write('        *    *       `  |  : :  |  .      ` \' :| | :| . : :         *   :.||'),nl,
+    write('             .`            | |  |  : .:|       ` | || | : |: |          | ||'),nl,
+    write('      \'          .         + `  |  :  .: .         \'| | : :| :    .   |:| ||'),nl,
+    write('         .                 .    ` *|  || :       `    | | :| | :      |:| |'),nl,
+    write(' .                .          .        || |.: *          | || : :     :|||'),nl,
+    write('        .            .   . *    .   .  ` |||.  +        + \'| |||  .  ||`'),nl,
+    write('     .             *              .     +:`|!             . ||||  :.||`'),nl,
+    write(' +                      .                ..!|*          . | :`||+ |||`'),nl,
+    write('     .                         +      : |||`        .| :| | | |.| ||`     .'),nl,
+    write('       *     +   \'               +  :|| |`     :.+. || || | |:`|| `'),nl,
+    write('                            .      .||` .    ..|| | |: \'` `| | |`  +'),nl,
+    write('  .       +++                      ||        !|!: `       :| |'),nl,
+    write('              +         .      .    | .      `|||.:      .||    .      .    `'),nl,
+    write('          \'                           `|.   .  `:|||   + ||\'     `'),nl,
+    write('  __    +      *                         `\'       `\'|.    `:'),nl,
+    write('"\'  `---"""----....____,..^---`^``----.,.___          `.    `.  .    ____,.,-'),nl,
+    write('    ___,--\'""`---"\'   ^  ^ ^        ^       """\'---,..___ __,..---""\''),nl,
+    write('--"\'                           ^                         ``--..,__ D. Rice'),nl,nl,
+    write('Dovahngard : hanaca... raka... datasawala... padhajaya... nyamagabat... hanga...'),nl,sleep(0.5),
+    write('Dovahngard : hanaca... raka...........'),nl,sleep(0.5),
+    write('Dovahngard : ....Oh dear.... Those visions are true......I see the light of Wangkygard inside your eyes...'),nl,sleep(0.5),
+    write('Options: i.  What do you mean by that, old man?'),nl,
+    write('         ii. Wangkygard...um what?'),
+    asserta(options(1)),asserta(questOnGoing(1)).
+
+/* Jika masih ada quest yang berjalan */
+foundQuest :- posPlayer(X,Y),(quest1(X,Y);quest2(X,Y);quest3(X,Y)),questOnGoing(1),
+    write('You are still in a quest, mortal. Finish it up first.'),!.
+
+
+opt :- posPlayer(X,Y),quest1(X,Y),questOnGoing(1),quest(1,1,1),options(1),
+    write('Options: i.   Alright. I\'ll show them this great Wangky Grandson wkwk.'),nl,
+    write('         ii.  My condolences. I will show them no mercy.'),nl,
+    write('         iii. Ahh they are just a bunch of dog yet you can\'t get rid of them. Alright alright.'),
+    retract(options(1)),asserta(options(2)).
+
+opt :- posPlayer(X,Y),quest2(X,Y),questOnGoing(1),quest(2,3,2),options(1),
+    write('Choose your dialogue option with command \'1\', \'2\'.'),nl,
+    write('Options: i.  I will gladly join our countrymen\'s battle.'),nl,
+    write('         ii. Alright alright'),
+    retract(options(1)),asserta(options(2)).
+
+i :- quest(1,1,1), options(1),!,
+    write('Veelfdir: My bianglala has been burned by those filthy monsters! Please, get rid of them all'), opt.
+i :- quest(1,1,1),options(2),!,
+    write('Veelfdir: Thank you, sir.'),nl,nl,
+    write('##############QUEST 1##############'),nl,
+    write('Kill 1 slime, 1 goblin, and 1 wolf.'),
+    retract(options(2)).
+i :- quest(2,3,2),options(1),!,
+    write('Laborers  : We are trying to accomplish your grandfather, Willy Wangky\'s goal,'),nl,
+    write('            to remove the impurities in this theme park. Will you join us, dear sir?'),opt.
+i :- quest(2,3,2),options(2),!,
+    write('Laborers  : Let\'s go with us then, sir.'),nl,
+    write('##############QUEST 2##############'),nl,
+    write('Kill 2 slimes, 3 goblins, 2 wolves'),
+    retract(options(2)).
+i :- quest(5,6,4),options(1),!,
+    write('Dovahngard : The prophecy...hanaca... has said that will come a man... whom descendant of the great Willy Wangky...'),nl,
+    write('             ...to break all the indecency by the darkness that has been corrupting the land of Willy Wangky...'),nl,
+    write('Dovahngard : ...I believe you are the one who could end the misery on this land... sir...'),nl,sleep(0.5),
+    write('Options: i. What should I do? I mean, all these monsters just appear with no ending sequence, old man.'),
+    retract(options(1)),asserta(options(2)).
+i :- quest(5,6,4),options(2),!,
+    write('Dovahngard : ...In the mean time... this old earthling just requests you to clean my Histeria from those impurities...'),nl,sleep(0.5),
+    write('             ...but those things will never end until you.....end the biggest impurities laid beneath this land....'),nl,
+    write('             ...God give me a hope to this land as the sunrise could be seen again...'),nl,sleep(0.5),
+    write('##############QUEST 3##############'),nl,
+    write('Kill 5 slimes, 6 goblins, and 4 wolves.').
+
+ii :- quest(1,1,1), options(1),!,
+    write('Veelfdir: My farm has been burned by those filthy monsters! Please, get rid of them all'), opt.
+ii :- quest(1,1,1),options(2),!,
+    write('Veelfdir: Thank you, dear sir. I hope you could came back with no dirty rags.'),nl,nl,
+    write('##############QUEST 1##############'),nl,
+    write('Kill 1 slime, 1 goblin, and 1 wolf.'),
+    retract(options(2)).
+ii :- quest(2,3,2),options(1),!,
+    write('Laborers  : Sunrise above, Willy Wangky\'s grandson. Are you interested to join our battle?'),opt.
+ii :- quest(2,3,2),options(2),!,
+    write('Laborers  : Sounds lazy... Anyway, let\'s go.'),nl,
+    write('##############QUEST 2##############'),nl,
+    write('Kill 2 slimes, 3 goblins, and 2 wolves'),
+    retract(options(2)).
+ii :- quest(5,6,4),options(1),!,
+    write('Dovahngard : Wangkygard.....The soul who once own this land...Now he appears, the descendant of the great Willy Wangky...'),nl,
+    write('           : ...to break all the indecency by the darkness that has been corrupting the land of Willy Wangky...'),nl,sleep(0.5),
+    write('Dovahngard : ...I believe you are the one who could end the misery on this land... sir...'),nl,sleep(0.5),
+    write('Options: [1] What should I do? I mean, all these monsters just appear with no ending sequence, old man.'),
+    retract(options(1)),asserta(options(2)).
+
+iii :- quest(1,1,1), options(1),!,
+    write('Veelfdir: And I oop...What happened that you make the darkness led behind your cruel words, sir?'),nl,
+    write('          I have no wares left. My farm has been burned by those filthy monsters!'), opt.
+iii :- quest(1,1,1),options(2),!,
+    write('Veelfdir: *silent*'),nl,nl,
+    write('##############QUEST 1##############'),nl,
+    write('Kill 1 slime, 1 goblin, and 1 wolf.'),
+    retract(options(2)).
 
 
 questFinish :- win(2,2,2),
